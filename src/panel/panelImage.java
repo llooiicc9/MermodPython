d73cd795a5c01ee6af45a0033698c563c7d8358a
loic <loic.python@gmail.com>
2018-05-25 11:37:38 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/llooiicc9/MermodPython.git
    
    Conflicts:
    	src/bouton/btnContact.java

--- src/panel/panelImage.java
+++ src/panel/panelImage.java
@@ -8,8 +8,14 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.DecimalFormat;
 import java.util.Date;
 
@@ -18,11 +24,13 @@ import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import bouton.btnBase;
 import bouton.btnUnlock;
@@ -30,7 +38,8 @@ import bouton.btnUnlock;
 public class panelImage extends JPanel 
 {
 	private JPanel gridPanel = new JPanel(new GridLayout(0, 3, 7, 7));
-	
+	private JButton btnAdd = new JButton("Add");
+	private String chemin="images/Gallerie";
 	
 	public panelImage() 
 	{
@@ -38,17 +47,35 @@ public class panelImage extends JPanel
 		
 		afficheImage();
 		
-		add(gridPanel);
+		add(btnAdd, BorderLayout.NORTH);
+		add(gridPanel, BorderLayout.CENTER);
+		
+		btnAdd.addActionListener(new ActionListener(){
+		   public void actionPerformed(ActionEvent event){				
+			   
+		        addImage();
+		      }
+	        });
 
 	}
-	
+
 	public void afficheImage()
 	{
 		JButton vignette;
-		for (int i = 1; i <= 8; i++)  
+
+		File dossiertest= new File(chemin);		
+		
+		if(dossiertest.isDirectory())
 		{
-			vignette = new btnBase("images/Gallerie/photo"+i+".jpg");
-			gridPanel.add(vignette);
+			System.out.println("Nombre de photo dans la galerie : "+dossiertest.list().length);
+			File list[] = dossiertest.listFiles();
+			for (int i = 0; i < list.length; i++) 
+			{
+				System.out.println(list[i].getName());
+				vignette = new btnBase("images/Gallerie/"+list[i].getName());
+				gridPanel.add(vignette);
+			}
+		
 		}
 		/*for (Photo photo : photos) 
 		{
@@ -60,16 +87,32 @@ public class panelImage extends JPanel
 		}*/
 	}
 	
-	public void paintComponent(Graphics g)
-    {
-    		try 
-    	    {
-    	      Image img = ImageIO.read(new File("images/background.jpg"));
-    	      //g.drawImage(img, 0, 0, this);
-    	      //Pour une image de fond
-    	     g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-    	    } catch (IOException e) {
-    	     System.out.println("ERREUR : le fond d'écran ne peut pas être chargé ");
-    	    }                
-    }
+	public void addImage()
+	{
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Open Resource File");
+		 
+		 FileNameExtensionFilter filter = new FileNameExtensionFilter(
+			        "JPG & GIF Images", "jpg", "gif","png");
+		 fileChooser.setFileFilter(filter);
+			    int returnVal = fileChooser.showOpenDialog(null);
+		
+			    if(returnVal == JFileChooser.APPROVE_OPTION) {
+			       System.out.println("Ajout du fichier : " + fileChooser.getSelectedFile().getName());
+			     
+			 
+			       Path cheminBase = Paths.get(fileChooser.getSelectedFile().getPath());
+			       Path cheminOuEnregistrer = Paths.get(chemin+"/"+fileChooser.getSelectedFile().getName());
+				   
+					try {
+						Files.copy(cheminBase, cheminOuEnregistrer, StandardCopyOption.REPLACE_EXISTING);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+		
+			    }
+	}
+	
 }
