d1fca006586210f1a218d0bc0dc0ec932d18b582
mermo <mermo@10.91.131.14>
2018-06-07 13:57:08 +0200

    ChikiChikitaaaaa

--- src/panel/panelHaut.java
+++ src/panel/panelHaut.java
@@ -3,6 +3,7 @@ package panel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
@@ -27,22 +28,31 @@ public class panelHaut extends JPanel {
 	private Timer timerHeure = new Timer(0, new CurrentTime());
 	private JLabel reseau = new JLabel("Swisscom 4G");
 	private JButton hp = new btnBase("images/top.png");
+	private Icon batery = new ImageIcon("images/battery.png");
+	private JLabel battery = new JLabel();
+	private JPanel a = new JPanel();
 	
 	
 	public panelHaut() {
 		
-		setLayout(new BorderLayout());
+		setLayout(new GridLayout(1,3));
 		setBackground(Color.black);
+		battery.setIcon(batery);
+		a.setLayout(new BorderLayout());
+		a.add(battery,BorderLayout.EAST);
+		a.setBackground(Color.BLACK);
 		
 		timerHeure.start();
 
 		lblHeure.setHorizontalAlignment(JLabel.CENTER);
 		lblHeure.setForeground(Color.WHITE);
-		lblHeure.setFont(new Font("Arial", Font.BOLD, 20));
+		lblHeure.setFont(new Font("Arial", Font.BOLD, 23));
 		reseau.setForeground(Color.WHITE);
-		reseau.setFont(new Font("Arial", Font.BOLD,20));
-		add(lblHeure, BorderLayout.CENTER);
-		add(reseau, BorderLayout.WEST);
+		reseau.setFont(new Font("Arial", Font.BOLD,23));
+		add(reseau);
+		add(lblHeure);
+		add(a, BorderLayout.EAST);
+		
 	
 		
 	}
